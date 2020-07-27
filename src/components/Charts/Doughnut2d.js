aa9f1b4bf9fd65ce0e108e19d16e50f7850fa8b6
krittiyaclark <krittiyaclark@gmail.com>
2020-07-27 16:54:51 -0400

    Displaying a Doughnut Chart  and calculating most used language

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,48 @@
-import React from 'react';
+// STEP 1 - Include Dependencies
+// Include react
+import React from 'react'
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
-};
+// Include the react-fusioncharts component
+import ReactFC from 'react-fusioncharts'
 
-export default Doughnut2d;
+// Include the fusioncharts library
+import FusionCharts from 'fusioncharts'
+
+// Include the chart type
+import Chart from 'fusioncharts/fusioncharts.charts'
+
+// Include the theme as fusion
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.candy'
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Chart, FusionTheme)
+
+// STEP 3 - Creating the JSON object to store the chart configurations
+
+const ChartComponent = ({ data }) => {
+	const chartConfigs = {
+		type: 'doughnut2d', // The chart type
+		width: '400', // Width of the chart
+		height: '400', // Height of the chart
+		dataFormat: 'json', // Data type
+		dataSource: {
+			// Chart Configuration
+			chart: {
+				caption: 'Stars Per Languages',
+				decimals: 0,
+				theme: 'candy',
+				pieRadius: '45%',
+				doughnutRadius: '60%',
+				showPercentValues: 0,
+				paletteColors:
+					'#2caeba, #5D62B5, #FFC533, #F2726F, #8d6e63, #1de9b6, #6E80CA',
+			},
+			// Chart Data
+			data,
+		},
+	}
+	return <ReactFC {...chartConfigs} />
+}
+// STEP 4 - Creating the DOM element to pass the react-fusioncharts component
+
+export default ChartComponent
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,28 +6,41 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts'
 const Repos = () => {
 	const { repos } = useContext(GithubContext)
 	let languages = repos.reduce((total, item) => {
-		const { language } = item
+		const { language, stargazers_count } = item
 
 		if (!language) return total // Check if language is null, return it
 		if (!total[language]) {
 			total[language] = {
 				label: language,
 				value: 1,
+				stars: stargazers_count,
 			}
 		} else {
 			total[language] = {
 				...total[language],
 				value: total[language].value + 1,
+				stars: total[language].stars + stargazers_count,
 			}
 		}
 		return total
 	}, {})
-	languages = Object.values(languages)
+
+	const mostUsed = Object.values(languages)
 		.sort((a, b) => {
 			return b.value - a.value
 		})
 		.slice(0, 5)
 
+	// most star per language
+	const mostpopular = Object.values(languages)
+		.sort((a, b) => {
+			return b.stars - a.stars
+		})
+		.map((item) => {
+			return { ...item, value: item.stars }
+		})
+		.slice(0, 5)
+	console.log(mostpopular)
 	const chartData = [
 		{
 			label: 'HTML',
@@ -45,8 +58,10 @@ const Repos = () => {
 	return (
 		<section className='section'>
 			<Wrapper className='section-center'>
-				<Pie3D data={languages} />
-				{/* <ExampleChart data={chartData} /> */}
+				<Pie3D data={mostUsed} />
+				<div></div>
+				<Doughnut2D data={mostpopular} />
+				<div></div>
 			</Wrapper>
 		</section>
 	)
