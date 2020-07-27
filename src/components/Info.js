88b2b38d90249c222f3b83609b3e8ed0768e9256
krittiyaclark <krittiyaclark@gmail.com>
2020-07-27 12:39:12 -0400

    Output a Info, Card, Followers components

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,15 +1,55 @@
 import React, { useContext } from 'react'
+
 import { GithubContext } from '../context/context'
 import styled from 'styled-components'
 import { GoRepo, GoGist } from 'react-icons/go'
 import { FiUsers, FiUserPlus } from 'react-icons/fi'
+import InfoItem from './InfoItem'
 
 const UserInfo = () => {
-	const data = useContext(GithubContext)
+	const { githubUser } = useContext(GithubContext)
+	const { public_repos, followers, following, public_gists } = githubUser
 
-	console.log(data)
+	const items = [
+		{
+			id: 1,
+			icon: <GoRepo className='icon' />,
+			label: 'repos',
+			value: public_repos,
+			color: 'pink',
+		},
+		{
+			id: 2,
+			icon: <FiUsers className='icon' />,
+			label: 'followers',
+			value: followers,
+			color: 'green',
+		},
+		{
+			id: 3,
+			icon: <FiUserPlus className='icon' />,
+			label: 'following',
+			value: following,
+			color: 'purple',
+		},
+		{
+			id: 4,
+			icon: <GoGist className='icon' />,
+			label: 'gists',
+			value: public_gists,
+			color: 'yellow',
+		},
+	]
 
-	return <h2>user info component</h2>
+	return (
+		<section className='section'>
+			<Wrapper className='section-center'>
+				{items.map((item) => {
+					return <InfoItem key={item.id} {...item}></InfoItem>
+				})}
+			</Wrapper>
+		</section>
+	)
 }
 
 const Wrapper = styled.section`
