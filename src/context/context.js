19191a2f751655114a74190b84791ad55430d9bd
krittiyaclark <krittiyaclark@gmail.com>
2020-07-27 21:18:28 -0400

    Fetch an API and set up an error message when exceeded a hourly rate limit

--- src/context/context.js
+++ src/context/context.js
@@ -12,9 +12,45 @@ const GithubProvider = ({ children }) => {
 	const [githubUser, setGithubUser] = useState(mockUser)
 	const [repos, setRepos] = useState(mockRepos)
 	const [followers, setFollowers] = useState(mockFollowers)
+	// request, loading
+	const [requests, setRequests] = useState(0)
+	const [isLoading, setIsLoading] = useState(false)
+	// error
+	const [error, setError] = useState({
+		show: false,
+		msg: '',
+	})
+	// check Requests
+	const checkRequests = () => {
+		axios(`${rootUrl}/rate_limit`)
+			.then(({ data }) => {
+				let {
+					rate: { remaining },
+				} = data
+				// remaining = 0
+				setRequests(remaining)
+				if (remaining === 0) {
+					// throw an error
+					toggleError(true, 'sorry, you have exceeded your hourly rate limit!')
+				}
+			})
+			.catch((error) => console.log(error))
+	}
 
+	function toggleError(show = false, msg = '') {
+		setError({ show, msg })
+	}
+
+	useEffect(checkRequests, [])
 	return (
-		<GithubContext.Provider value={{ githubUser, repos, followers }}>
+		<GithubContext.Provider
+			value={{
+				githubUser,
+				repos,
+				followers,
+				requests,
+				error,
+			}}>
 			{children}
 		</GithubContext.Provider>
 	)
